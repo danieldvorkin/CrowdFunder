7194816ca7c6a898d1f3b6dc8629a607eeb190c1
ikhakoo <ikhakoo@gmail.com>
2015-05-12 15:10:59 -0400

    fixed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	validates :name, presence: true
 	has_many :projects
 	has_many :rewards, through: :projects
 	has_many :pledges, through: :projects
