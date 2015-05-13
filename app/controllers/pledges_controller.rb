78efdda4482d63c4a47f7087b1fb6c9fd0fe7c8a
ikhakoo <ikhakoo@gmail.com>
2015-05-13 15:46:00 -0400

    fixed pledges button

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -6,8 +6,8 @@ class PledgesController < ApplicationController
 
 	def create
     @project = load_project
-    @pledge = @product.pledges.build(pledge_params)
-    @pledge.user = current_user
+    @pledge = @project.pledges.build(pledge_params)
+    @pledge.user_id = current_user.id
 
     respond_to do |format|
       if @pledge.save
@@ -23,7 +23,7 @@ class PledgesController < ApplicationController
 
 private
   def pledge_params
-    params.require(:pledge).permit(:reward_id)
+    {reward_id: params[:reward_id]}
   end
 
   def load_project
